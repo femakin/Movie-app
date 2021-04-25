47c34cf80088a2e3313167042e60e492254a127d
Femi Akinyemi <santosfemakin@yahoo.com>
2021-04-25 13:55:31 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MoviesList from './components/MoviesList';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+<h1>Movie Search App</h1>
+
+<MoviesList/>
+
+      <footer>
+	<p>
+		Created with <FontAwesomeIcon icon={faHeart} /> by
+		<a target="_blank" href="https://femi-akinyemi.netlify.app" rel="noreferrer">Femi Akinyemi</a>
+		{/* - Read
+		<a target="_blank" rel="noreferrer" href="https://www.florin-pop.com/blog/2019/02/react-movie-search-app/">how I created it</a>
+		- Other projects on
+		<a target="_blank" rel="noreferrer" href="https://github.com/florinpop17/work-journal/">
+			Github
+		</a> */}
+	</p>
+</footer>
     </div>
   );
 }
